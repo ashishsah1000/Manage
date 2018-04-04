9fd5961e6ce289ff726dbb1bc7e41e5f61d42b7f
Ashish Sah <ashishsah1000@gmail.com>
2018-04-04 13:33:57 +0000

    added the things from c9 get the qr link

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
+    var id,url;
+    console.log("ready");
+    $(".link").css("display","none");
+    var fLink=false;
+    $(".getLink").click(function(){
+        var par = $(this).parent().siblings("#id");
+        console.log(par);
+        id = par[0].innerText;
+        console.log(id );
+        url="https://es6-ashishsah1000.c9users.io/upd?id="+id+"&month=feb";
+       
+        $("#qrlink").html(url);
+         $(".link").css("display","block");
+    });
+    
 });
